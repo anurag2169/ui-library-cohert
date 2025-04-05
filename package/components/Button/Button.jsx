c0e0ee2f1b1b2efb6962a7c2642fc49787ef2443
Anurag Dubey <anuragd2169@gmail.com>
2025-04-05 13:28:25 +0530

    add tailwind globally theme

--- package/components/Button/Button.jsx
+++ package/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const Button = () => {
   return (
-    <button className="bg-primary py-4 rounded-xl text-primaryText">
+    <button className="bg-primary text-primary-foreground px-4 py-2 rounded-xl border-2 border-border hover:brightness-110 transition">
       Button From Package
     </button>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,31 @@
+import colors from "tailwindcss/colors";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./package/**/*.{js,jsx,ts,tsx}"], // Only scan your library files
   theme: {
     extend: {
       colors: {
+        background: "rgb(var(--background))",
+        foreground: "rgb(var(--foreground))",
+
         primary: "rgb(var(--primary))",
+        primaryForeground: "rgb(var(--primary-foreground))",
+
         secondary: "rgb(var(--secondary))",
-        primaryText: "rgb(var(--primary-text))",
-        secondaryText: "rgb(var(--secondary-text))",
-        text: "rgb(var(--text))",
-        linkText: "rgb(var(--link-text))",
+        secondaryForeground: "rgb(var(--secondary-foreground))",
+
+        muted: "rgb(var(--muted))",
+        mutedForeground: "rgb(var(--muted-foreground))",
+
+        accent: "rgb(var(--accent))",
+        accentForeground: "rgb(var(--accent-foreground))",
+
+        destructive: "rgb(var(--destructive))",
+        destructiveForeground: "rgb(var(--destructive-foreground))",
+
+        border: "rgb(var(--border))",
+        input: "rgb(var(--input))",
+        ring: "rgb(var(--ring))",
       },
     },
   },
