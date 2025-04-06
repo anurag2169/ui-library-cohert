6510a833acf9aa764ca07c937ada901fa69b637a
salswa <swatindp@gmail.com>
2025-04-06 15:25:08 +0530

    test commit

--- package/components/Button/Button.jsx
+++ package/components/Button/Button.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export const Button = () => {
   return (
     <button className="bg-primary text-primary-foreground px-4 py-2 rounded-xl border-2 border-border hover:brightness-110 transition">
-      Button From Package
+      Button From Package ui_library
     </button>
   );
 };
